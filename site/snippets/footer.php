e5610dc5df547d82e351d4840db1383b154238e6
Sean M <sean@workbypost.com>
2018-12-05 10:58:24 +0000

    added gtag

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,45 +1,55 @@
 
-  <footer class="footer">
+    <footer class="footer">
     
-    <div class="footer_left">
+        <div class="footer_left">
 
-      <a href="/" class="footer_wordmark anim">
-        <?php snippet('wordmark') ?>
-      </a>
+            <a href="/" class="footer_wordmark anim">
+                <?php snippet('wordmark') ?>
+            </a>
 
-      <p class="footer_copyright anim"><?php echo html::decode($site->copyright()->kirbytext()) ?></p>
+            <p class="footer_copyright anim"><?php echo html::decode($site->copyright()->kirbytext()) ?></p>
 
-    </div>
+        </div>
 
-    <div class="footer_right">
+        <div class="footer_right">
 
-      <ul class="footer_nav anim">
-        <?php foreach($site->footerlinks()->toStructure() as $link): ?>
-        <li class="footer_<?php echo $link->page(); ?>link">
-          <a href="/<?php echo $link->page() ?>" alt="<?php echo $link->title() ?>">
+            <ul class="footer_nav anim">
+            <?php foreach($site->footerlinks()->toStructure() as $link): ?>
+            <li class="footer_<?php echo $link->page(); ?>link">
+            <a href="/<?php echo $link->page() ?>" alt="<?php echo $link->title() ?>">
             <?php echo $link->title() ?>
             <span><?php echo $link->tagline() ?></span>
-          </a>
-        </li>
-        <?php endforeach; ?>
-      </ul>
+            </a>
+            </li>
+            <?php endforeach; ?>
+            </ul>
 
-      <ul class="footer_social anim">
-        <li><a href="#" alt="Twitter"><?php snippet('twitter') ?></a></li>
-        <li><a href="#" alt="Facebook"><?php snippet('fb') ?></a></li>
-        <li><a href="#" alt="Instagram"><?php snippet('insta') ?></a></li>
-        <li><a href="#" alt="LinkedIn"><?php snippet('linkedin') ?></a></li>
-      </ul>
+            <ul class="footer_social anim">
+            <li><a href="#" alt="Twitter"><?php snippet('twitter') ?></a></li>
+            <li><a href="#" alt="Facebook"><?php snippet('fb') ?></a></li>
+            <li><a href="#" alt="Instagram"><?php snippet('insta') ?></a></li>
+            <li><a href="#" alt="LinkedIn"><?php snippet('linkedin') ?></a></li>
+            </ul>
 
-    </div>
+        </div>
 
-  </footer>
+    </footer>
 
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAejlVkvJTY813eXwYB8ERoCm5sxSDlPKo&center=53.34653234395884,-6.24379321083677"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAejlVkvJTY813eXwYB8ERoCm5sxSDlPKo&center=53.34653234395884,-6.24379321083677"></script>
 
-  <?= js('assets/build/all.js') ?>
-  <?= js('assets/build/app.js') ?>
-  <?php // js('assets/scripts/app.min.js') ?>
+    <?= js('assets/build/all.js') ?>
+    <?= js('assets/build/app.js') ?>
+    <?php // js('assets/scripts/app.min.js') ?>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-72690519-3"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-72690519-3');
+    </script>
+
 
 </body>
 </html>
