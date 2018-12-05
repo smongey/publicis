39deedd78c0007fa45a180bf234db68c05431d92
Sean M <sean@workbypost.com>
2018-12-05 23:06:39 +0000

    client logos

--- site/templates/clients.php
+++ site/templates/clients.php
@@ -5,8 +5,13 @@
       
       <h3 class="clients_title">Our clients</h3>
 
-      <?php echo $page->clients()->kt(); ?>
-      
+      <!-- <?php //echo $page->clients()->kt(); ?> -->
+    
+    <ul class="clients_logos">
+    <?php foreach($page->images() as $i): ?>
+        <li><img src="<?php echo $i->url(); ?>" alt=""></li>
+    <?php endforeach; ?>
+    </ul>
     </section>
   </main>
 
