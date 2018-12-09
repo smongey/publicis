b3c031a69b90c21a0aac95cde4e9f6bbcc0a8990
Sean M <sean@workbypost.com>
2018-12-09 15:11:03 +0000

    removed our clients heading

--- site/templates/clients.php
+++ site/templates/clients.php
@@ -3,7 +3,7 @@
   <main class="clients">
     <section>
       
-      <h3 class="clients_title">Our clients</h3>
+      <!-- <h3 class="clients_title">Our clients</h3> -->
 
       <!-- <?php //echo $page->clients()->kt(); ?> -->
     
