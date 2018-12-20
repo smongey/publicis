ff24027dc45e46fa957078d19ed3157626f54059
Sean M <sean@workbypost.com>
2018-12-20 12:59:44 +0000

    updated seo

--- site/snippets/seo.php
+++ site/snippets/seo.php
@@ -3,23 +3,26 @@
     $seoDesc = $page->seoDesc()->isNotEmpty() ? $page->seoDesc() : $site->description();
     $seoImage = $page->seoImage()->isNotEmpty() ? thumb($page->image($page->seoImage()), array('width'=> 1200, 'height'=> 630, 'crop'=>true))->url() : false;
 ?>
-<meta name="description" content="<?php echo $seoDesc; ?>">
 
+    <meta name="description" content="<?php echo $seoDesc; ?>">
+
+    <!-- OG -->
     <meta property="og:site_name" content="<?php echo $site->title()->html() ?>">
     <meta property="og:title" content="<?php echo $seoTitle; ?>">
     <meta property="og:description" content="<?php echo $seoDesc; ?>">
     <meta property="og:url" content="<?php echo $page->url() ?>">
-    <?php if($seoImage == false): ?>
+<?php if($seoImage): ?>
     <meta property="og:image" content="<?php echo $seoImage; ?>">
-    <?php endif; ?>
+<?php endif; ?>
     <meta property="og:type" content="website">
 
+    <!-- Twitter -->
     <meta name="twitter:title" content="<?php echo $seoTitle; ?>">
     <meta name="twitter:description" content="<?php echo $seoDesc; ?>">
     <meta name="twitter:url" content="<?php echo $page->url() ?>">
-    <?php if($seoImage == false): ?>
+<?php if($seoImage): ?>
     <meta name="twitter:image" content="<?php echo $seoImage; ?>">
-    <?php endif; ?>
+<?php endif; ?>
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="<?php echo $site->twitter()->html() ?>">
     <meta name="twitter:creator" content="<?php echo $site->twitter()->html() ?>">
